711039b0aa22d2c31076781f1ac5ca82615bce62
WK <wksim91@yahoo.com.sg>
2016-02-24 12:44:58 +0800

    Change pw to "p@ssword"

--- php_func/functions.php
+++ php_func/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 $dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres
-password=cs2102")
+password=p@ssword")
 or die('Could not connect: ' . pg_last_error());
 
   function first($int, $string){ //function parameters, two variables.
