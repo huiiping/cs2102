a5ef5463721f12bccd15da6cd7ab5b8efaf90929
yeojoey <yeojoey@gmail.com>
2016-02-28 18:50:23 +0800

    Make functional

--- php_func/additem.php
+++ php_func/additem.php
@@ -1,8 +1,5 @@
 <?php
 
-	$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=p@ssword")
-	or die('Could not connect:' . pg_last_error());
-
 	include('php_func/functions.php');
 
 	$owner = $_SESSION['login_user'];
