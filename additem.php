a148dd5f7831aa52718d073bb5a748108b80b0a1
yeojoey <yeojoey@gmail.com>
2016-03-01 16:41:45 +0800

    add footer include

--- additem.php
+++ additem.php
@@ -50,47 +50,9 @@
 			</form>
     </div>
     <!-- end of center content -->
-    <div class="right_content">
-      <div class="title_box">Search</div>
-      <div class="border_box">
-        <input type="text" name="newsletter" class="newsletter_input" value="keyword"/>
-        <a href="#" class="join">search</a> </div>
-      <div class="shopping_cart">
-        <div class="title_box">Shopping cart</div>
-        <div class="cart_details"> 3 items <br />
-          <span class="border_cart"></span> Total: <span class="price">350$</span> </div>
-        <div class="cart_icon"><a href="#"><img src="images/shoppingcart.png" alt="" width="35" height="35" border="0" /></a></div>
-      </div>
-      <div class="title_box">What’s new</div>
-      <div class="border_box">
-        <div class="product_title"><a href="#">Motorola 156 MX-VL</a></div>
-        <div class="product_img"><a href="#"><img src="images/p2.jpg" alt="" border="0" /></a></div>
-        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
-      </div>
-      <div class="title_box">Manufacturers</div>
-      <ul class="left_menu">
-        <li class="odd"><a href="#">Bosch</a></li>
-        <li class="even"><a href="#">Samsung</a></li>
-        <li class="odd"><a href="#">Makita</a></li>
-        <li class="even"><a href="#">LG</a></li>
-        <li class="odd"><a href="#">Fujitsu Siemens</a></li>
-        <li class="even"><a href="#">Motorola</a></li>
-        <li class="odd"><a href="#">Phillips</a></li>
-        <li class="even"><a href="#">Beko</a></li>
-      </ul>
-      <div class="banner_adds"> <a href="#"><img src="images/bann1.jpg" alt="" border="0" /></a> </div>
-    </div>
-    <!-- end of right content -->
-  </div>
-  <!-- end of main content -->
-  <div class="footer">
-    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="89" height="42"/> </div>
-    <div class="center_footer"> Template name. All Rights Reserved 2008<br />
-      <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
-      <img src="images/payment.gif" alt="" /> </div>
-    <div class="right_footer"> <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="#">contact us</a> </div>
-  </div>
-</div>
-<!-- end of main_container -->
-</body>
-</html>
+
+<?php
+
+  include("includes/footer.php");
+
+?>
