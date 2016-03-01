46c8b3cfdb548132dee45367b61386c2b99228a6
tsneo <ts_neo89@hotmail.com>
2016-03-01 10:08:57 -0800

    Edited the login page

--- loginpage.php
+++ loginpage.php
@@ -1,49 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Tools Shop</title>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-<!--[if IE 6]>
-<link rel="stylesheet" type="text/css" href="iecss.css" />
-<![endif]-->
-<script type="text/javascript" src="js/boxOver.js"></script>
-</head>
-<body>
-
 <?php
-session_start();
+
+	$page_title = "Add Item";
+	
+	include("includes/header.php");
+
+	if(isset($_SESSION["login_user"])){
+		header("location: index.php");
+		exit();
+	}
+	
 ?>
-
-<?php
-include_once("login.php"); // Includes Login Script
-
-if(isset($_SESSION["login_user"])){
-	header("location: index.php");
-	exit();
-}
-?>
-
-
-<div id="main_container">
-  <div id="header">
-    <div class="top_right">
-      <div class="languages">
-        <div class="lang_text">Languages:</div>
-        <a href="#" class="lang"><img src="images/en.gif" alt="" border="0" /></a> <a href="#" class="lang"><img src="images/de.gif" alt="" border="0" /></a> </div>
-      <div class="big_banner"> <a href="#"><img src="images/banner728.jpg" alt="" border="0" /></a> </div>
-    </div>
-    <div id="logo"> <a href="#"><img src="images/logo.png" alt="" border="0" width="182" height="85" /></a> </div>
-  </div>
-  <div id="main_content">
-    <div id="menu_tab">
-      <ul class="menu">
-        <li><a href="logout.php" class="nav">Logout</a></li>
-        <li class="divider"></li>
-        <li><a href="register.php" class="nav">Sign Up</a></li>
-        <li class="divider"></li>
-      </ul>
-    </div>
 
     <div class="center_content">
       <div class="center_title_bar">Login</div>
