4aa9e55ac11cfa8c790dbe644fb1a9408479e6d2
huiiping <hpong_95@hotmail.com>
2016-03-21 22:19:37 +0800

    Update Register

--- register.php
+++ register.php
@@ -1,44 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Registration</title>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-<!--[if IE 6]>
-<link rel="stylesheet" type="text/css" href="iecss.css" />
-<![endif]-->
-<script type="text/javascript" src="js/boxOver.js"></script>
-</head>
-<body>
-<div id="main_container">
-  <div id="header">
-    <div class="top_right">
-      <div class="languages">
-        <div class="lang_text">Languages:</div>
-        <a href="#" class="lang"><img src="images/en.gif" alt="" border="0" /></a> <a href="#" class="lang"><img src="images/de.gif" alt="" border="0" /></a> </div>
-      <div class="big_banner"> <a href="#"><img src="images/banner728.jpg" alt="" border="0" /></a> </div>
-    </div>
-    <div id="logo"> <a href="#"><img src="images/logo.png" alt="" border="0" width="182" height="85" /></a> </div>
-  </div>
-  <div id="main_content">
-    <div id="menu_tab">
-      <ul class="menu">
-        <?php
-			if($_SESSION["login_user"]) {
-				echo '<li><a href="logout.php" class="nav">Logout</a></li>';
-			}
-			else{
-				echo '<li><a href="loginpage.php" class="nav">Login</a></li>';
-			}
-		?>
-        <li class="divider"></li>
-        <li><a href="register.php" class="nav">Sign Up</a></li>
-        <li class="divider"></li>
-      </ul>
-    </div>
+<?php
+
+	$page_title = "Registration";
+	
+	include("includes/header.php");
+?>
 	<div class="center_content">
 	  <div class="center_title_bar">User Registration</div>
-	  <center>
 		<form action="register.php" method="post">
 			<table align="center"> 
 			  <tr>
@@ -76,17 +43,10 @@ include_once('php_func\registration.php');
 ?>
 
 		</form>
-	  </center>
 	</div>
 	<!-- end of center content -->
-    <div class="footer">
-    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="89" height="42"/> </div>
-    <div class="center_footer"> Template name. All Rights Reserved 2008<br />
-      <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
-      <img src="images/payment.gif" alt="" /> </div>
-    <div class="right_footer"> <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="#">contact us</a> </div>
-  </div>
-</div>
-<!-- end of main_container -->
-</body>
-</html>
+<?php
+
+  include("includes/footer.php");
+
+?>
