8b048ff6bc3e9f25178b1fdb8a2d2c3837662fce
WK <wksim91@yahoo.com.sg>
2016-04-01 21:46:25 +0800

    Returned Items will now be displayed in Index

--- return.php
+++ return.php
@@ -8,16 +8,23 @@ session_start();
 
 $itemID=$_GET['itemID'];
 
+$convert_startDate = date("Y/m/d  H:i:s");
+
 $query = "UPDATE item SET availability='TRUE'
+			WHERE itemID = '".$itemID."';";
+			
+$query2 = "UPDATE item_to_bid SET startdate= '".$convert_startDate."'
 			WHERE itemID = '".$itemID."';";
 
 		$result = pg_query($query);
+		$result2 = pg_query($query2);
 		
-		if (!$result) {
+		if (!$result && !$result2) {
 				echo "Not returned.";
 		} else {
 			echo "Returned!";
 		}
+
 
 header("Location: /mylistings.php#personalitems");
 die();
