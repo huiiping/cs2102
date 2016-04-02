354be25af06f3c7fe092abf2522d2d5ad66f8996
tsneo <ts_neo89@hotmail.com>
2016-04-01 20:34:39 -0700

    updated the templates
    modified the php files to include the admin templates

--- admin_edit_user.php
+++ admin_edit_user.php
@@ -1,69 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Update User Particular</title>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-<!--[if IE 6]>
-<link rel="stylesheet" type="text/css" href="iecss.css" />
-<![endif]-->
-<script type="text/javascript" src="js/boxOver.js"></script>
-<?php include 'php_func\functions.php'; ?>
-<?php 
-	session_start();
-?>
 <?php
-if($_SESSION["login_user"] && $_SESSION["logon_type"] == "ADMIN") {
-		?>
+
+	$page_title = "Manual update Winners";
+	
+	include("includes/admin_header.php");
+	
+	if($_SESSION["login_user"]){
 		
-		<?php
-		}
-		else {
-			header("location: loginpage.php");
-		}
+	}
+	else{
+		header("location: loginpage.php");
+	}
+	include 'php_func\functions.php';
 ?>
-</head>
-<body>
-<div id="main_container">
-  <div id="header">
-    <div class="top_right">
-      <div class="languages">
-        <div class="lang_text">Languages:</div>
-        <a href="#" class="lang"><img src="images/en.gif" alt="" border="0" /></a> <a href="#" class="lang"><img src="images/de.gif" alt="" border="0" /></a> </div>
-      <div class="big_banner"> <a href="#"><img src="images/banner728.jpg" alt="" border="0" /></a> </div>
-    </div>
-    <div id="logo"> <a href="#"><img src="images/logo.png" alt="" border="0" width="182" height="85" /></a> </div>
-  </div>
-  <div id="main_content">
-    <div id="menu_tab">
-      <ul class="menu">
-        <li><a href="#" class="nav"> Home </a></li>
-        <li class="divider"></li>
-        <li><a href="mylistings.php" class="nav">My Listing</a></li>
-        <li class="divider"></li>
-        <li><a href="mybiditems.php" class="nav">Bidding</a></li>
-        <li class="divider"></li>
-		<li><a href="additem.php" class="nav">Add Item</a></li>
-        <li class="divider"></li>
-        <li><a href="logout.php" class="nav">Logout</a></li>
-        <li class="divider"></li>
-        <li><a href="register.php" class="nav">Sign Up</a></li>
-        <li class="divider"></li>
-      </ul>
-    </div>
-    <!-- end of menu tab -->
-    <div class="crumb_navigation"> Navigation: <span class="current">Update User Particular</span> </div>
-    <div class="left_content">
-      <div class="title_box">Menu</div>
-      <ul class="left_menu">
-        <li class="odd"><a href="admin_manage_users.php">Manage Users</a></li>
-        <li class="odd"><a href="admin_manage_items.php">Manage Items</a></li>
-        <li class="even"><a href="admin_search_info.php">Search Information</a></li>
-      </ul>
-      <div class="border_box">
-      </div>
-    </div>
-    <!-- end of left content -->
 	<!-- Insert form here -->
     <div class="center_content">
       <div class="center_title_bar">Update User Details</div>
@@ -114,14 +62,8 @@ if($_SESSION["login_user"] && $_SESSION["logon_type"] == "ADMIN") {
     <!-- end of right content -->
   </div>
   <!-- end of main content -->
-  <div class="footer">
-    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="89" height="42"/> </div>
-    <div class="center_footer"> Template name. All Rights Reserved 2008<br />
-      <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
-      <img src="images/payment.gif" alt="" /> </div>
-    <div class="right_footer"> <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="#">contact us</a> </div>
-  </div>
-</div>
-<!-- end of main_container -->
-</body>
-</html>
+  <?php
+
+	include("includes/admin_footer.php");
+
+	?>
